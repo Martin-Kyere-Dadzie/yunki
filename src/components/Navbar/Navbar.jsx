654aca2ad755cdfb26a0f91753579ee40823b739
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2024-02-03 17:53:25 +0000

    creating the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Navbar.scss';
 import yunkiLogoIcon from '../../public/icons/yunki-white-logo.svg';
-import searchIcon from '../../public/icons/search-gray-icon.svg';
+import searchIcon from '../../public/icons/search-white-icon.svg';
 import userIcon from '../../public/icons/user-icon.svg';
 import bagIcon from '../../public/icons/shopping-bag-icon.svg';
 import heartIcon from '../../public/icons/heart-icon.svg';
@@ -14,17 +14,38 @@ const Navbar = () => {
             <img className="main-logo" src={yunkiLogoIcon} alt="logo-icon"></img>
         </div>
         <div className="search__section">
-            <img className="search-icon" src={searchIcon} alt="search-icon"></img>
+            <input 
+                type='search' 
+                className='input' 
+                placeholder="search all your products here..."
+            ></input>
+            <img 
+                className="search-icon" 
+                src={searchIcon} 
+                alt="search-icon"
+            ></img>
         </div>
         <div className="user-icons-navigation__section">
-            <div className="userIcon__section">            
-                <img className="user-icon" src={userIcon} alt="user-icon"></img>
+            <div className="userIcon__section nav-icon">            
+                <img 
+                    className="user-icon" 
+                    src={userIcon} 
+                    alt="user-icon"
+                ></img>
             </div>
-            <div className="shoppingBag__section">            
-                <img className="bag-icon" src={bagIcon} alt="bag-icon"></img>
+            <div className="shoppingBag__section nav-icon">            
+                <img 
+                    className="bag-icon" 
+                    src={bagIcon} 
+                    alt="bag-icon"
+                ></img>
             </div>
-            <div className="heartIcon__section">            
-                <img className="heart-icon" src={heartIcon} alt="search-icon"></img>
+            <div className="heartIcon__section nav-icon">            
+                <img 
+                    className="heart-icon" 
+                    src={heartIcon} 
+                    alt="search-icon"
+                ></img>
             </div>
         </div>
     </div>
