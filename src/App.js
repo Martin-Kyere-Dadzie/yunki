1a18d90e152e340a2f09eb8c73b9a8def01a90e2
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2024-02-04 01:01:27 +0000

    navbar section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Checkout from './components/Checkout/Checkout';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
+          <Route path='/checkout' element={<Checkout/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,47 +5,68 @@ import searchIcon from '../../public/icons/search-white-icon.svg';
 import userIcon from '../../public/icons/user-icon.svg';
 import bagIcon from '../../public/icons/shopping-bag-icon.svg';
 import heartIcon from '../../public/icons/heart-icon.svg';
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        <div className="logo__section">
-            <img className="main-logo" src={yunkiLogoIcon} alt="logo-icon"></img>
+        <div className="navbar-news__section">
+            <p className="news-text">
+                Shop on Yunki for Exclusive Offers and Discounts-Absolute Limited Offers.
+            </p>
         </div>
-        <div className="search__section">
-            <input 
-                type='search' 
-                className='input' 
-                placeholder="search all your products here..."
-            ></input>
-            <img 
-                className="search-icon" 
-                src={searchIcon} 
-                alt="search-icon"
-            ></img>
-        </div>
-        <div className="user-icons-navigation__section">
-            <div className="userIcon__section nav-icon">            
-                <img 
-                    className="user-icon" 
-                    src={userIcon} 
-                    alt="user-icon"
-                ></img>
+        <div className="navbar-icons__section">
+            <div className="logo__section">
+                <NavLink to="/">                 
+                    <img 
+                        className="main-logo" 
+                        src={yunkiLogoIcon} 
+                        alt="logo-icon"
+                    ></img>
+                </NavLink>
             </div>
-            <div className="shoppingBag__section nav-icon">            
+            <div className="search__section">
+                <input 
+                    type='search' 
+                    className='input' 
+                    placeholder="search all your products here..."
+                ></input>
                 <img 
-                    className="bag-icon" 
-                    src={bagIcon} 
-                    alt="bag-icon"
-                ></img>
-            </div>
-            <div className="heartIcon__section nav-icon">            
-                <img 
-                    className="heart-icon" 
-                    src={heartIcon} 
+                    className="search-icon" 
+                    src={searchIcon} 
                     alt="search-icon"
                 ></img>
+            </div>
+            <div className="user-icons-navigation__section">
+                <div className="userIcon__section nav-icon">            
+                    <img 
+                        className="user-icon" 
+                        src={userIcon} 
+                        alt="user-icon"
+                    ></img>
+                    <h5 className="icon-text"><span>Create</span>Account</h5>
+                </div>
+                <NavLink className="nav-icon" to="/Checkout">                
+                    <div className="shoppingBag__section">            
+                        <img 
+                            className="bag-icon" 
+                            src={bagIcon} 
+                            alt="bag-icon"
+                        ></img>
+                        <h5 className="icon-text"><span>Order</span>Checkout</h5>
+                    </div>
+                </NavLink>
+                <NavLink className="nav-icon">                
+                    <div className="heartIcon__section">            
+                        <img 
+                            className="heart-icon" 
+                            src={heartIcon} 
+                            alt="search-icon"
+                        ></img>
+                        <h5 className="icon-text"><span>Like</span>Wishlist</h5>
+                    </div>
+                </NavLink>
             </div>
         </div>
     </div>
