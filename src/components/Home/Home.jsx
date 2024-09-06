f7601c32a438b290690c6fb508bcb819426c8940
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2024-09-06 09:46:19 +0100

    home stores responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -23,27 +23,27 @@ const Home = () => {
   return (
     <div className="home-section">
       <div className="stores-shortcut__links">
-        <NavLink to="/pets"  className="store-shortcut pets">
+        <NavLink to="/pets"  className="store-shortcut pets" id="pet-store">
           <div className="imageText-container">
             <img className="petImage" src={PetsImage} alt="pet"></img>
             <h5>pets days save big</h5>
             <p></p>
           </div>
         </NavLink>
-        <NavLink to="/women" className="store-shortcut women">
+        <NavLink to="/women" className="store-shortcut women" id="women-store">
           <div className="imageText-container">
             <img className="womenImage" src={womenImage} alt="womenImage"></img>
             <h5>save on women fashion & more</h5>
             <p></p>
           </div>
         </NavLink>
-        <NavLink to="/electronics" className="store-shortcut electronics">
+        <NavLink to="/electronics" className="store-shortcut electronics" id="electronic-store">
           <div className="imageText-container">
             <img className="electronicsImage" src={electronicImage} alt="electronics"></img>
             <h5>electronics</h5>
           </div>
         </NavLink>
-        <NavLink to="/food" className="store-shortcut food">
+        <NavLink to="/food" className="store-shortcut food" id="food-store">
           <div className="imageText-container">
             <img className="foodImage" src={foodImage} alt="food"></img>
             <h5>
@@ -51,19 +51,19 @@ const Home = () => {
             </h5>
           </div>
         </NavLink>
-        <NavLink to="/men" className="store-shortcut men">
+        <NavLink to="/men" className="store-shortcut men" id="men-store">
           <div className="imageText-container">
             <img className="menImage" src={menImage} alt="men"></img>
             <h5>low prices on men fashion and more</h5>
           </div>
         </NavLink>  
-        <NavLink to="/fashionAndBeauty" className="store-shortcut fashion-and-beauty">
+        <NavLink to="/fashionAndBeauty" className="store-shortcut fashion-and-beauty" id="fashion-store">
           <div className="imageText-container">
             <img className="skinCareImage" src={skinCareImage} alt="skincare"></img>
             <h5>skin care</h5>
           </div>
         </NavLink>
-        <NavLink to="/phonesAndLaptops" className="store-shortcut phones-laptops">
+        <NavLink to="/phonesAndLaptops" className="store-shortcut phones-laptops" id="phones-laptops">
           <img className="phoneImage" src={phonesAndLaptops} alt="phones"></img>
           <div className="imageText-container">
             <h5>phones, laptops and accessories</h5>
@@ -71,7 +71,7 @@ const Home = () => {
         </NavLink>
         <NavLink to="/kids" className="store-shortcut kids">
           <div className="imageText-container">
-            <img className="kidsImage" src={kidsImage} alt="kidsImage"></img>
+            <img className="kidsImage" src={kidsImage} alt="kidsImage" id="kids-store"></img>
             <h5>kids</h5>
           </div>
         </NavLink>
