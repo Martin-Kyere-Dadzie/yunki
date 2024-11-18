b8571e06136a8c2145f13a80a07413443f70efd4
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2024-11-18 12:53:15 +0100

    offer icons

--- src/components/ActiveConfig/ActiveConfig.jsx
+++ src/components/ActiveConfig/ActiveConfig.jsx
@@ -1,7 +1,7 @@
 export const activeLinkStyle = ({ isActive }) => {
     return {
-        borderBottom: isActive ? '1px solid #ff5500': 'none',
-        color: isActive ? '#ff5500' : '#575757',
+        borderBottom: isActive ? '1px solid #3F94A6': 'none',
+        color: isActive ? '#3F94A6' : '#575757',
         // fontFamily: isActive ? 'poppinsBold' : 'poppinsMedium'
     }
 }
