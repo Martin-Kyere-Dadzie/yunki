b3b943745445abe713b2c082a15770be46522b2c
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2024-12-29 23:08:36 +0100

    pet shop front end

--- src/components/PetsShop/PetsShop.jsx
+++ src/components/PetsShop/PetsShop.jsx
@@ -6,18 +6,29 @@ const PetsShop = () => {
   return (
     <div className='pets__banner__section'>
       <div className='big__banner'>
-        <div className='pets__banner__img banner__item'>
-          <img alt='img' src={petsBannerImage}></img>
+        <div className='slider__holder'>
+          <div className='pets__banner__img banner__item'>
+            <img alt='img' src={petsBannerImage}></img>
+          </div>
+          <div className='pets__banner__text banner__item'>
+            <h2>Proident in cupidatat incididunt elit aute consectetur cupidatat ea veniam irure .</h2>
+            <button className='banner__btn'>shop now</button>
+          </div>
         </div>
-        <div className='pets__banner__text banner__item'>
-          <h2>Proident in cupidatat incididunt elit aute consectetur cupidatat ea veniam irure .</h2>
-          <button className='banner__btn'>shop now</button>
+        <div className='link__holder'>
+          <div className='link one'>
+            <img alt='img' src={catImag}></img>
+            <h2>Satisfy Your Cat with Quality Cat Foods</h2>
+          </div>
+          <div className='link two'>
+            <img alt='img' src={dogImage}></img>
+            <h2>Save Big, Make Pet Giggle And Wiggle!</h2>
+          </div>
+          <div className='link three'>
+            <img alt='img' src={birdImage}></img>
+            <h2>Enjoy Discounts On Nutritious Bird Foods</h2>
+          </div>
         </div>
-      </div>
-      <div className='link__holder'>
-        <div className='link one'></div>
-        <div className='link two'></div>
-        <div className='link three'></div>
       </div>
       </div>
   )
