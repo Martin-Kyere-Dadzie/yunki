cbd74dd469c0719febe62808565e507a0a62da97
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2024-12-30 03:07:54 +0100

    pet sop images added

--- src/components/PetsShop/PetsShop.jsx
+++ src/components/PetsShop/PetsShop.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './PetsShop.scss';
-import petsBannerImage from '../../public/images/pngaaa.com-3037481.png'
+import petsBannerImage from '../../public/images/hamster-basket-isolated-white-table_2829-19759.png'
+import catImage from '../../public/images/bengal-cat-studio_87557-3561.png';
+import dogImage from '../../public/images/young-jack-russel-terrier-toys_87557-7792.png'
+import birdImage from '../../public/images/ara-ararauna-studio_87557-2719.png'
 
 const PetsShop = () => {
   return (
@@ -17,12 +20,12 @@ const PetsShop = () => {
         </div>
         <div className='link__holder'>
           <div className='link one'>
-            <img alt='img' src={catImag}></img>
-            <h2>Satisfy Your Cat with Quality Cat Foods</h2>
-          </div>
-          <div className='link two'>
             <img alt='img' src={dogImage}></img>
             <h2>Save Big, Make Pet Giggle And Wiggle!</h2>
+          </div>
+          <div className='link two'>
+            <img alt='img' src={catImage}></img>
+            <h2>Satisfy Your Cat with Quality Cat Foods</h2>
           </div>
           <div className='link three'>
             <img alt='img' src={birdImage}></img>
