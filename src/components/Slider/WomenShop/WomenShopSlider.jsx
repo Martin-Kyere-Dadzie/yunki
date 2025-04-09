edb7f61d6dbf2fbb221ba71378676ae96909abfe
martin-kyere-dadzie <abeikudadzie5@gmail.com>
2025-04-09 02:03:13 +0100

    slider changes

--- src/components/Slider/WomenShop/WomenShopSlider.jsx
+++ src/components/Slider/WomenShop/WomenShopSlider.jsx
@@ -1,10 +1,39 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { womenSliderData } from './WomenSliderData';
 import './WomenShopSlider.scss';
 
 const WomenShopSlider = () => {
-    const [currentSlide, setCurrentSlide] = useState(0)
-  return (
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideLength = womenSliderData.length;
+
+    const autoScroll = true;
+    let slideInterval;
+    let intervalTime = 5000;
+
+    const nexSlide = () => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+    }
+
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1)
+    }
+
+    function auto(){
+        slideInterval = setInterval(nexSlide, intervalTime)
+    }
+
+    useEffect(() => {
+        setCurrentSlide(0)
+    }, []);
+
+    useEffect(() => {
+        if (autoScroll) {
+            auto()
+        }
+        return() => clearInterval(slideInterval);
+    }, [currentSlide]);
+
+    return (
     <div className='slider'>
         {
             womenSliderData.map((slide, index) => {
